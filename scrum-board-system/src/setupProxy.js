ce164c84317f200bb4048d973872f2a9282090b9
Fernando Velasco Salazar <fernando.velasco@coppel.com>
2020-08-19 10:32:04 -0500

    Initial commit, initial dependencies added

---
+++ scrum-board-system/src/setupProxy.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express();
+
+module.exports = function(app) {
+  app.use(
+    '/apiprojects',
+    createProxyMiddleware({
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      pathRewrite:{'/apiprojects':''}
+    })
+  );
+  app.use(
+    '/apiuserstories',
+    createProxyMiddleware({target:'http://localhost:8080',
+    changeOrigin:true,
+    pathRewrite:{'/apiuserstories':''}
+  })
+  );
+};
