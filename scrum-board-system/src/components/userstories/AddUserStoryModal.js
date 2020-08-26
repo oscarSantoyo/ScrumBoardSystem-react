d664aab24d27864c10b8832697459b54fb03d7ac
Oscar Santoyo <oscar.santoyo@wizeline.com>
2020-08-26 15:16:44 -0500

    Story modal

--- scrum-board-system/src/components/userstories/AddUserStoryModal.js
+++ scrum-board-system/src/components/userstories/AddUserStoryModal.js
@@ -1,29 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Button, Modal, Form, FormControl, InputGroup, Dropdown, DropdownButton } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { connect } from 'react-redux'
 import { addUserStory } from '../../actions/userstories'
 
 const Sprint = ({sprint, sprints, register }) => {
+  console.log('Sprints', sprints)
+  const [sprintValue, setSprintValue] = useState(sprint?.name)
+
+  const onChange = (change) => {console.log('This changed', change)
+                               setSprintValue(sprints.find(spr => spr.id == change).name)
+                              }
     return (
-<InputGroup className="mb-3">
-    <DropdownButton
-      as={InputGroup.Prepend}
-      variant="outline-secondary"
-      title="Sprint"
-      id="input-group-dropdown-1"
-    >
-      <Dropdown.Item href="#">Action</Dropdown.Item>
-      <Dropdown.Item href="#">Another action</Dropdown.Item>
-      <Dropdown.Item href="#">Something else here</Dropdown.Item>
-      <Dropdown.Divider />
-      <Dropdown.Item href="#">Separated link</Dropdown.Item>
-    </DropdownButton>
-    <FormControl aria-describedby="basic-addon1" value="hola" />
-  </InputGroup>
+      <div className="form-group row">
+        <InputGroup>
+          <DropdownButton
+            as={InputGroup.Prepend}
+            variant="outline-secondary"
+            title="Sprint"
+            id="input-group-dropdown-1"
+            onSelect={onChange}
+          >
+            {sprints && sprints.map(sprintObj => (
+              <Dropdown.Item key={sprintObj.id} eventKey={sprintObj.id}> {sprintObj.name}</Dropdown.Item>
+            ))}
+          </DropdownButton>
 
-
-
+          <div className="col-sm-2">
+            {sprintValue &&
+             <a id="SprintId" className="badge badge-pill badge-primary"> {sprintValue} </a>
+            }
+          </div>
+          <FormControl type="text" readOnly hidden aria-describedby="basic-addon1" ref={register} name="sprint" value={ sprintValue } />
+        </InputGroup>
+      </div>
     )
 }
 
@@ -42,29 +52,37 @@ const LabelContainer = ({labels}) => {
     )
 }
 
+const Task = ({task, onChange}) => {
+  const [taskState, setTaskState] = useState(task)
+  return (
+    <div key={taskState?.id} class="input-group mb-3">
+      <div class="input-group-prepend">
+        <div class="input-group-text">
+          <input type="checkbox" aria-label="Checkbox for following text input" checked={taskState?.done}/>
+        </div>
+      </div>
+      <input type="text" class="form-control" aria-label="Text input with checkbox" value={taskState?.description}/>
+    </div>
+  )
+}
 const TasksContainer = ({tasks}) => {
-
-  const tasksMutated = [{id:12 }]
+  const tasksMutated = [{id:12, description: 'task 12', done:true }
+                        , {id:1, description: 'task 1', done: false}]
   console.log('mutated', tasksMutated)
     return (
       <div class="form-group row align-items-center">
         <div class="col-sm-2">Tasks</div>
         {tasksMutated && tasksMutated.map(task => (
-          <div key={task.id} class="input-group mb-3">
-            <div class="input-group-prepend">
-              <div class="input-group-text">
-                <input type="checkbox" aria-label="Checkbox for following text input"/>
-              </div>
-            </div>
-            <input type="text" class="form-control" aria-label="Text input with checkbox"/>
-          </div>
+          <Task
+          task={task}/>
         ))}
+        <Task/>
       </div>
     )
 }
 
 const UserStory = (props) => {
-  const {title, description, weight, labels, tasks, sprint, id, onSubmit } = props
+  const {title, description, weight, labels, tasks, sprint, sprints, id, onSubmit } = props
   const { handleSubmit, register, error} = useForm()
   return (
     <>
@@ -87,7 +105,7 @@ const UserStory = (props) => {
           <input type="text" class="form-control" ref={register} name="weight" id="weight" value={weight}/>
         </div>
       </div>
-      <Sprint sprint={sprint} />
+      <Sprint sprint={sprint} sprints={sprints} register={register} />
       <LabelContainer labels={labels}/>
       <TasksContainer tasks={tasks}/>
       <input type="submit" className="btn btn-primary" value="ADD"/>
@@ -95,10 +113,11 @@ const UserStory = (props) => {
         </>
   )
 }
-const AddUserStory = ({project, show, handleClose, handleShow, addUserStory})=> {
+const AddUserStory = ({project, sprints, show, handleClose, handleShow, addUserStory})=> {
   const onSubmit = (values) => {
-    handleClose()
-    addUserStory(project.id, values)
+    console.log('values',  values)
+    //handleClose()
+    //addUserStory(project.id, values)
   }
   return (
       <Modal show={show} onHide={handleClose}
@@ -109,6 +128,7 @@ const AddUserStory = ({project, show, handleClose, handleShow, addUserStory})=> 
         <Modal.Body>
           <UserStory
             onSubmit={onSubmit}
+            sprints={sprints}
           />
         </Modal.Body>
         <Modal.Footer>
